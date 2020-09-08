a5787f4084bf96c35aac42d46cc72b1d330dd6e7
El-Potato-Slayer <leighchad.cl@gmail.com>
2020-09-08 09:58:30 +0200

    added board and tictactoe class

---
+++ lib/board.rb
@@ -0,0 +1,10 @@
+class
+  # attr_reader :matrix
+  # def initialize
+  #   @matrix = [
+  #     ['O', ' ', ' '],
+  #     [' ', 'X', ' '],
+  #     [' ', ' ', 'O']
+  #   ]
+  # end
+end
---
+++ lib/tictactoe.rb
@@ -0,0 +1,12 @@
+class TicTacToe
+  attr_reader :player1, :player2, :board  
+  def initialize(player1, player2, board)
+    @player1 = player1
+    @player2 = player2
+    @board = board
+  end
+
+  def print_board
+    #returns string of board
+  end
+end
